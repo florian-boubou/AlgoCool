0e9b00dc20e5c0b2e76aab6d9d4164063e2e774c
Unknown <MorgulHeat76@gmail.com>
2017-01-06 09:46:55 +0100

    Ajout chemin de compilation

--- ProgrammeProjet/src/Test.java
+++ ProgrammeProjet/src/Test.java
@@ -1,31 +1,34 @@
 import java.io.*;
 import java.util.ArrayList;
 
-public class Test {
-    public static void main(String[] args){
-        ArrayList<String> code = new ArrayList<String>();
-        String line = null;
+public class Test
+{
+	public static void main( String[] args )
+	{
+		ArrayList<String> code = new ArrayList<String>();
+		String            line = null;
 
-        try {
-            //InputStream ips = Test.class.getResourceAsStream("../test.algo");
-            //InputStreamReader ipsr = new InputStreamReader(ips);
-            FileReader fr = new FileReader("test.algo");
-            BufferedReader br = new BufferedReader(fr);
-            while((line = br.readLine()) != null) {
-                code.add(line);
-            }
-        }
-        catch(Exception e) {
-            System.out.println(e);
-        }
+		try
+		{
+			//InputStream ips = Test.class.getResourceAsStream("../test.algo");
+			//InputStreamReader ipsr = new InputStreamReader(ips);
+			FileReader     fr = new FileReader( "data/test.algo" );
+			BufferedReader br = new BufferedReader( fr );
 
-        SyntaxChecker sc = new SyntaxChecker(code);
-        System.out.println("Test Header :");
-        System.out.println(sc.headerCheck());
-        System.out.println("Test Data :");
-        System.out.println(sc.dataCheck());
-        System.out.println("Test Body :");
-        System.out.println(sc.bodyCheck());
+			while ( ( line = br.readLine() ) != null )
+				code.add( line );
 
-    }
+		} catch ( Exception e )
+		{
+			e.printStackTrace();
+		}
+
+		SyntaxChecker sc = new SyntaxChecker( code );
+		System.out.println( "Test Header :" );
+		System.out.println( sc.headerCheck() );
+		System.out.println( "Test Data :" );
+		System.out.println( sc.dataCheck() );
+		System.out.println( "Test Body :" );
+		System.out.println( sc.bodyCheck() );
+	}
 }

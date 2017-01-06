835142aca2070ff68300b9fbb665a62b855022d7
Florian Boulant <florian.boulant@orange.fr>
2017-01-06 09:28:58 +0100

    Amélioration HashMapConfig et addition du CodeStyle

--- ProgrammeProjet/src/HashMapConfig.java
+++ ProgrammeProjet/src/HashMapConfig.java
@@ -1,43 +1,54 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.HashSet;
 
 /**
  * Created by Terawa on 2016/12/29.
  */
-public class HashMapConfig {
-    private HashMap<String, HashSet<String>> reserved;
+public class HashMapConfig
+{
+	private HashMap<String, HashSet<String>> reserved;
 
-    public HashMapConfig() {
-        reserved = new HashMap<String, HashSet<String>>();
-        String line = null;
-        String key = null;
 
-        try {
-            InputStream ips = Test.class.getResourceAsStream( "test.algo" );
-            FileReader fr = new FileReader();
-            BufferedReader br = new BufferedReader();
-            while((line = br.readLine()) != null) {
-                line.replaceAll("\\t| ", "");
-                if (line.matches("^\\/\\*.*")) {
-                    key = line.substring(2);
-                    reserved.put(key, new HashSet<String>());
-                }
-                else {
-                    if (!line.matches("^\\/\\/.*") && !line.equals("\n")) {
-                        reserved.get(key).add(line);
-                    }
-                }
-            }
-        }
-        catch(Exception e) {
-            System.out.println(e);
-        }
-    }
+	public HashMapConfig()
+	{
+		reserved = new HashMap<String, HashSet<String>>();
+		String line = null;
+		String key  = null;
 
-    public HashMap<String, HashSet<String>> getHashMapConfig() {
-        return reserved;
-    }
+		try
+		{
+			InputStream       ips  = Test.class.getResourceAsStream( "test.algo" );
+			InputStreamReader ipsr = new InputStreamReader( ips );
+			BufferedReader    br   = new BufferedReader( ipsr );
+			while ( ( line = br.readLine() ) != null )
+			{
+				line.replaceAll( "\\t| ", "" );
+				if ( line.matches( "^\\/\\*.*" ) )
+				{
+					key = line.substring( 2 );
+					reserved.put( key, new HashSet<String>() );
+				}
+				else
+				{
+					if ( ! line.matches( "^\\/\\/.*" ) && ! line.equals( "\n" ) )
+					{
+						reserved.get( key ).add( line );
+					}
+				}
+			}
+		} catch ( Exception e )
+		{
+			System.out.println( e );
+		}
+	}
+
+
+	public HashMap<String, HashSet<String>> getHashMapConfig()
+	{
+		return reserved;
+	}
 }

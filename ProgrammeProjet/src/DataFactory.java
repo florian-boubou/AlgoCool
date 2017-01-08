94e480e39d6048ce99dd99b27c9c9a19c9a05842
Unknown <waretant@gmail.com>
2017-01-08 23:57:45 +0900

    DataFactory
    Création des variables + modification de la classe Regex

--- ProgrammeProjet/src/DataFactory.java
+++ ProgrammeProjet/src/DataFactory.java
@@ -1,6 +1,77 @@
+import engine.type.*;
+import tool.Regex;
+
+import java.util.HashMap;
 /**
  * Created by Terawa on 08/01/2017.
  */
 public class DataFactory {
+	private enum Type {
+		INTEGER("entier", Regex.REGEX_INTEGER),
+		DOUBLE("double",Regex.REGEX_DOUBLE),
+		CHAR("caractere", Regex.REGEX_CHAR),
+		STRING("string", Regex.REGEX_STRING),
+		BOOLEAN("booleen", Regex.REGEX_BOOLEAN);
+
+		private String pCType;
+		private String regexType;
+
+		Type(String pcType, String regexType) {
+			this.pCType = pcType;
+			this.regexType = regexType;
+		}
+
+		public String getpCType() {
+			return pCType;
+		}
+
+		public String getRegexType() {
+			return regexType;
+		}
+	}
+	private HashMap<String ,Variable> hMapData;
+
+	public DataFactory() {
+		hMapData = new HashMap<>();
+	}
+
+	public void dataDeclaration(String varName, String type, String value) throws Exception{
+		if(type == null) {
+			type = determineType(value);
+		}
+		switch(type) {
+			case "entier" :
+				hMapData.put(varName,new IntegerVar(varName, value));
+				break;
+			case "double" :
+				hMapData.put(varName,new DoubleVar(varName, value));
+				break;
+			case "caractere" :
+				hMapData.put(varName,new CharVar(varName, value));
+				break;
+			case "chaine" :
+				hMapData.put(varName,new StringVar(varName, value));
+				break;
+			case "booleen" :
+				hMapData.put(varName,new BooleanVar(varName, value));
+				break;
+			default :
+				throw new Exception("Erreur: type invalide");
+		}
+	}
+
+	private String determineType(String varName) {
+		String ret = null;
+		for(Type t : Type.values()) {
+			if(varName.matches(t.getRegexType())) {
+				ret = t.getpCType();
+			}
+		}
+		return ret;
+	}
+
+	public HashMap<String, Variable> getHMapData() {
+		return hMapData;
+	}
 
 }
--- ProgrammeProjet/src/tool/Regex.java
+++ ProgrammeProjet/src/tool/Regex.java
@@ -4,18 +4,25 @@ package tool;
  * Created by Terawa on 06/01/2017.
  */
 public class Regex {
+    public static final String REGEX_VARIABLE = "^[a-z][0-9A-Za-z]*(_[0-9a-zA-Z]*)*$";
+    public static final String REGEX_CONSTANT = "^[A-Z][0-9A-Z]*(_[0-9A-Z]*)*$";
+    public static final String REGEX_INTEGER = "[0-9]*";
+    public static final String REGEX_DOUBLE = "[0-9]*,[0-9]+";
+    public static final String REGEX_BOOLEAN = "^true|false$";
+    public static final String REGEX_STRING = "^\"([^\"]|\\\\\")*\"$";
+    public static final String REGEX_CHAR = "^'[^']'$";
 
-    public static boolean isVariable(String s) {return s.matches("^[a-z][0-9A-Za-z]*((-|_)[0-9a-zA-Z]*)*$");}
+    public static boolean isVariable(String s) {return s.matches(REGEX_VARIABLE);}
 
-    public static boolean isConstant(String s) {return s.matches("^[A-Z][0-9A-Z]*(_[0-9A-Z]*)*$");}
+    public static boolean isConstant(String s) {return s.matches(REGEX_CONSTANT);}
 
-    public static boolean isInteger(String s) {return s.matches("[0-9]*");}
+    public static boolean isInteger(String s) {return s.matches(REGEX_INTEGER);}
 
-    public static  boolean isDouble(String s) {return s.matches("[0-9]*,[0-9]+");}
+    public static  boolean isDouble(String s) {return s.matches(REGEX_DOUBLE);}
 
-    //public static boolean isBoolean(String s) {return s.matches();}
+    public static boolean isBoolean(String s) {return s.matches(REGEX_BOOLEAN);}
 
-    public static boolean isString(String s) {return s.matches("^\"([^\"]|\\\\\")*\"$");}
+    public static boolean isString(String s) {return s.matches(REGEX_STRING);}
 
-    public static boolean isCharacter(String s) {return s.matches("^'[^']'$");}
+    public static boolean isCharacter(String s) {return s.matches(REGEX_CHAR);}
 }

770893647d776a607d7af4af5178145d19d75550
Florian Boulant <florian.boulant@orange.fr>
2017-01-09 20:00:28 +0100

    Conditions parfaitement opérationnelles
    Correction du problème avec les opérateurs logiques binaires

--- ProgrammeProjet/src/AlgoInterpreter.java
+++ ProgrammeProjet/src/AlgoInterpreter.java
@@ -15,221 +15,292 @@ import java.util.HashMap;
  * @version 1.0.0a
  * @date 01/08/2017
  */
-public class AlgoInterpreter {
-    private Interpreter interpreter;
-    private DataFactory df;
-    private SyntaxChecker syntaxChecker;
-    private DataFactory data;
+public class AlgoInterpreter
+{
+	private Interpreter   interpreter;
+	private DataFactory   df;
+	private SyntaxChecker syntaxChecker;
+	private DataFactory   data;
 
-    private ArrayList<String> algorithm;
-    private ArrayList<Variable> alData;
-    private ArrayList<Boolean> conditionsStack;
+	private ArrayList<String>   algorithm;
+	private ArrayList<Variable> alData;
+	private ArrayList<Boolean>  conditionsStack;
 
-    /**
-     * Constructeur d'AlgoInterpreter
-     *
-     * @param algorithm L'ArrayList<String> représentant l'algorithme à interpréter
-     */
-    public AlgoInterpreter(ArrayList<String> algorithm) {
-        try {
-            interpreter = new Interpreter();
-            df = new DataFactory();
-            syntaxChecker = new SyntaxChecker(algorithm);
-            this.algorithm = algorithm;
-            alData = new ArrayList<>();
-            conditionsStack = new ArrayList<>();
-        } catch (Exception e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-
-    /**
-     * Méthode lançant l'interprétation
-     */
-    public void run() {
-        syntaxChecker.dataCheck();
-        this.declareData(syntaxChecker.gethData());
-        for (String s : df.getHMapData().keySet()) {
-            alData.add(df.getHMapData().get(s));
-        }
-        for(Variable v : alData){
-            try{
-                interpreter.eval(v.getName() + " = " + v.getStrValue());
-            }
-            catch(EvalError e){
-                System.err.println(e.toString());
-            }
-        }
-        for (String s : syntaxChecker.getBody()) {
-            this.processLine(s);
-        }
-    }
+	/**
+	 * Constructeur d'AlgoInterpreter
+	 *
+	 * @param algorithm L'ArrayList<String> représentant l'algorithme à interpréter
+	 */
+	public AlgoInterpreter( ArrayList<String> algorithm )
+	{
+		try
+		{
+			interpreter = new Interpreter();
+			df = new DataFactory();
+			syntaxChecker = new SyntaxChecker( algorithm );
+			this.algorithm = algorithm;
+			alData = new ArrayList<>();
+			conditionsStack = new ArrayList<>();
+		} catch( Exception e )
+		{
+			System.err.println( e.getMessage() );
+		}
+	}
 
 
-    /**
-     * Méthode stockant toutes les variables déclarées dans la DataFactory
-     *
-     * @param data L'ArrayList<String> correspondante à la partie données de l'algorithme
-     */
-    public void declareData(HashMap<String, String> data) {
-
-        for (String valName : data.keySet()) {
-            if (Regex.isConstant(valName)) {
-                try {
-                    df.dataDeclaration(valName, null, data.get(valName));
-                } catch (Exception e) {
-                    System.err.println(e.toString());
-                }
-            } else {
-
-                try {
-                    df.dataDeclaration(valName, data.get(valName), null);
-                } catch (Exception e) {
-                    System.err.println(e.toString());
-                }
-            }
-        }
-    }
-
-
-    /**
-     * Méthodant interprétant l'algorithme ligne par ligne
-     *
-     * @param line La ligne à interpréter
-     * @return Ce qui sera possiblement à afficher après l'interprétation de cette ligne
-     */
-    public String processLine(String line) {
-        line = line.trim();
-        System.out.println(line); //TEST
-        if (line.matches("^fsi$")){
-            conditionsStack.remove(conditionsStack.size() - 1);
-        }
-        else if (line.matches("^sinon$"))
-        {
-            Boolean b = conditionsStack.get(conditionsStack.size() - 1);
-            b = !b;
-        }
-
-        if (conditionsStack.isEmpty() || !conditionsStack.isEmpty() && conditionsStack.get(conditionsStack.size() - 1)) {
-            if (tool.Regex.isFunction(line)) {
-                String[] fonc = line.split("\\(");
-                switch (fonc[0]) {
-                    case "ecrire":
-                        return write(fonc[1].replace(')', ' ').trim());
-                    default:
-                        break;
-                }
-            } else if (line.indexOf("◄—") != -1) {
-                this.assignement(line);
-            } else if (tool.Regex.isCondition(line)) {
-                line = new String(line.replaceAll("\\s+", " ")).trim();
-                line = line.substring(line.indexOf("si") + 2, line.indexOf("alors")).trim();
-                this.conditionsStack.add(this.evaluateCondition(line));
-            }
-        }
+	/**
+	 * Méthode lançant l'interprétation
+	 */
+	public void run()
+	{
+		syntaxChecker.dataCheck();
+		this.declareData( syntaxChecker.gethData() );
+		for( String s : df.getHMapData().keySet() )
+		{
+			alData.add( df.getHMapData().get( s ) );
+		}
+		for( Variable v : alData )
+		{
+			try
+			{
+				interpreter.eval( v.getName() + " = " + v.getStrValue() );
+			} catch( EvalError e )
+			{
+				System.err.println( e.toString() );
+			}
+		}
+		for( String s : syntaxChecker.getBody() )
+		{
+			this.processLine( s );
+		}
+	}
 
 
-        return null;
-    }
+	/**
+	 * Méthode stockant toutes les variables déclarées dans la DataFactory
+	 *
+	 * @param data L'ArrayList<String> correspondante à la partie données de l'algorithme
+	 */
+	public void declareData( HashMap<String, String> data )
+	{
 
-    public void assignement(String line) {
-        String var = line.split("◄—")[0].trim();
-        String val = line.split("◄—")[1].trim();
+		for( String valName : data.keySet() )
+		{
+			if( Regex.isConstant( valName ) )
+			{
+				try
+				{
+					df.dataDeclaration( valName, null, data.get( valName ) );
+				} catch( Exception e )
+				{
+					System.err.println( e.toString() );
+				}
+			} else
+			{
 
-        for (Variable v : alData) {
-            if (v.getName().equals(var)) {
-                v.setValue(val);
-                try{
-                    interpreter.eval(v.getName() + " = " + v.getStrValue());
-                }
-                catch(EvalError e){
-                    System.err.println(e.toString());
-                }
-                System.out.println("AFFECTATION -> " + val + " | " + v.getName() + " : " + v.getStrValue()); //TEST
-            }
-        }
-    }
+				try
+				{
+					df.dataDeclaration( valName, data.get( valName ), null );
+				} catch( Exception e )
+				{
+					System.err.println( e.toString() );
+				}
+			}
+		}
+	}
 
-    public Boolean evaluateCondition(String condition) {
-        condition = condition.replaceAll("/=", "!=");
-        condition = condition.replaceAll("ET|et", "&&");
-        condition = condition.replaceAll("OU|ou", "||");
 
-        String[] tabS = condition.split("\\&\\&|\\|\\|");
-        condition = "";
+	/**
+	 * Méthodant interprétant l'algorithme ligne par ligne
+	 *
+	 * @param line La ligne à interpréter
+	 * @return Ce qui sera possiblement à afficher après l'interprétation de cette ligne
+	 */
+	public String processLine( String line )
+	{
+		line = line.trim();
+		System.out.println( line ); //TEST
+		if( line.matches( "^fsi$" ) )
+		{
+			conditionsStack.remove( conditionsStack.size() - 1 );
+		} else if( line.matches( "^sinon$" ) )
+		{
+			Boolean b = conditionsStack.get( conditionsStack.size() - 1 );
+			b = !b;
+		}
 
-        for(int i =0; i<tabS.length;i++)
-        {
-            if(tabS[i].matches("^\\w*\\s*=\\s*\\w*$"))
-                tabS[i]=tabS[i].replaceAll("=","==");
-            condition+=tabS[i];
-        }
+		if( conditionsStack.isEmpty() || !conditionsStack.isEmpty() && conditionsStack.get(
+				conditionsStack.size() - 1 ) )
+		{
+			if( tool.Regex.isFunction( line ) )
+			{
+				String[] fonc = line.split( "\\(" );
+				switch( fonc[0] )
+				{
+					case "ecrire":
+						return write( fonc[1].replace( ')', ' ' ).trim() );
+					default:
+						break;
+				}
+			} else if( line.indexOf( "◄—" ) != -1 )
+			{
+				this.assignement( line );
+			} else if( tool.Regex.isCondition( line ) )
+			{
+				line = new String( line.replaceAll( "\\s+", " " ) ).trim();
+				line = line.substring( line.indexOf( "si" ) + 2, line.indexOf( "alors" ) ).trim();
+				this.conditionsStack.add( this.evaluateCondition( line ) );
+			}
+		}
 
-        try {
-            return (Boolean) interpreter.eval(condition);
-        } catch (EvalError evalError) {
-            evalError.printStackTrace();
-        }
-        return false;
-    }
 
-    /**
-     * Méthode qui gère la primitive "ecrire"
-     *
-     * @param toWrite La chaîne représentant ce qu'il y a à écrire
-     * @return La chaîne de ce qui sera écrit
-     */
-    public String write(String toWrite) {
-        return this.process(toWrite);
-    }
+		return null;
+	}
 
-    /**
-     * Méthode permettant d'évaluer une expression passée en paramètre
-     *
-     * @param statement L'expression à évaluer
-     * @return L'évaluation de l'expression
-     */
-    public String process(String statement) {
-        String processed = null;
+	/**
+	 * Méthode permettant d'interpréter les lignes d'affectation de variables
+	 *
+	 * @param line La ligne comprenant l'affectation
+	 */
+	public void assignement( String line )
+	{
+		String var = line.split( "◄—" )[0].trim();
+		String val = line.split( "◄—" )[1].trim();
 
-        try {
-            if (Regex.isOperation(statement))
-                processed = (String) interpreter.eval(statement);
-        } catch (EvalError e) {
-            System.err.print(e.getErrorText());
-        }
+		for( Variable v : alData )
+			if( v.getName().equals( var ) )
+			{
+				v.setValue( val );
+				try
+				{
+					interpreter.eval( v.getName() + " = " + v.getStrValue() );
+				} catch( EvalError e )
+				{
+					System.err.println( e.toString() );
+				}
+			}
+	}
 
-        return processed;
-    }
+	/**
+	 * Méthode permettant d'évaluer une expression booléenne donnée en pseudo-code
+	 *
+	 * @param condition L'expression booléenne en pseudo-code
+	 * @return La valeur de l'expression booléenne à évaluer
+	 */
+	public Boolean evaluateCondition( String condition )
+	{
+		//Transformations classiques pseudo-code -> Java
+		condition = condition.replaceAll( "/=", "!=" );
+		condition = condition.replaceAll( "ET|et", "&&" );
+		condition = condition.replaceAll( "OU|ou", "||" );
 
-    /**
-     * Méthode permettant d'obtenir une ArrayList<Variable> représentant toutes les données utilisées dans l'algorithme
-     *
-     * @return Une ArrayList<Variable> représentant toutes les données utilisées dans l'algorithme
-     */
-    public ArrayList<Variable> getAlData() {
-        return alData;
-    }
+		//On stocke les opérateurs logiques pour les réassigner plus tard
+		ArrayList<String> logicOps = new ArrayList<>();
+		{
+			String tmp = new String( condition );
+			int    opET, opOU;
 
-    /**
-     * Point d'entrée du programme d'interprétation
-     *
-     * @param args Contiendra une chaîne représentant le chemin vers le fichier contenant l'algorithme à interpréter
-     */
-    public static void main(String[] args) {
-        AlgoReader algoReader = new AlgoReader("/test.algo");
-        AlgoInterpreter algoInterpreter;
-        SyntaxChecker syntaxChecker = null;
+			for( ; (opET = tmp.indexOf( "&&" )) != -1 || (opOU = tmp.indexOf( "||" )) != -1; )
+			{
+				if( opET == -1 )
+					logicOps.add( "||" );
+				else if( opOU == -1 )
+					logicOps.add( "&&" );
+				else if( opET < opOU )
+					logicOps.add( "&&" );
+				else
+					logicOps.add( "||" );
 
-        try {
-            syntaxChecker = new SyntaxChecker(algoReader.getAlgorithm());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (syntaxChecker.headerCheck() && syntaxChecker.dataCheck() /*&& syntaxChecker.bodyCheck()*/) {
-            algoInterpreter = new AlgoInterpreter(algoReader.getAlgorithm());
-            algoInterpreter.run();
-        }
-    }
+				tmp = tmp.replace( "&&", "" ).replace( "||", "" );
+			}
+		}
+
+		String[] tabS = condition.split( "\\&\\&|\\|\\|" );
+		condition = "";
+
+		//Transformation du = (pseudo-code) en == (pseudo-code)
+		for( int i = 0; i < tabS.length; i++ )
+		{
+			if( tabS[i].trim().matches( "^\\w*\\s*=\\s*\\w*$" ) )
+				tabS[i] = tabS[i].replaceAll( "=", "==" );
+			condition += tabS[i] + (i < tabS.length - 1 ? logicOps.get( i ) : "");
+		}
+
+		try
+		{
+			return (Boolean) interpreter.eval( condition );
+		} catch( EvalError evalError )
+		{
+			evalError.printStackTrace();
+		}
+		return false;
+	}
+
+	/**
+	 * Méthode qui gère la primitive "ecrire"
+	 *
+	 * @param toWrite La chaîne représentant ce qu'il y a à écrire
+	 * @return La chaîne de ce qui sera écrit
+	 */
+	public String write( String toWrite )
+	{
+		return this.process( toWrite );
+	}
+
+	/**
+	 * Méthode permettant d'évaluer une expression passée en paramètre
+	 *
+	 * @param statement L'expression à évaluer
+	 * @return L'évaluation de l'expression
+	 */
+	public String process( String statement )
+	{
+		String processed = null;
+
+		try
+		{
+			if( Regex.isOperation( statement ) )
+				processed = (String) interpreter.eval( statement );
+		} catch( EvalError e )
+		{
+			System.err.print( e.getErrorText() );
+		}
+
+		return processed;
+	}
+
+	/**
+	 * Méthode permettant d'obtenir une ArrayList<Variable> représentant toutes les données utilisées dans l'algorithme
+	 *
+	 * @return Une ArrayList<Variable> représentant toutes les données utilisées dans l'algorithme
+	 */
+	public ArrayList<Variable> getAlData()
+	{
+		return alData;
+	}
+
+	/**
+	 * Point d'entrée du programme d'interprétation
+	 *
+	 * @param args Contiendra une chaîne représentant le chemin vers le fichier contenant l'algorithme à interpréter
+	 */
+	public static void main( String[] args )
+	{
+		AlgoReader      algoReader    = new AlgoReader( "/test.algo" );
+		AlgoInterpreter algoInterpreter;
+		SyntaxChecker   syntaxChecker = null;
+
+		try
+		{
+			syntaxChecker = new SyntaxChecker( algoReader.getAlgorithm() );
+		} catch( Exception e )
+		{
+			e.printStackTrace();
+		}
+		if( syntaxChecker.headerCheck() && syntaxChecker.dataCheck() /*&& syntaxChecker.bodyCheck()*/ )
+		{
+			algoInterpreter = new AlgoInterpreter( algoReader.getAlgorithm() );
+			algoInterpreter.run();
+		}
+	}
 }
